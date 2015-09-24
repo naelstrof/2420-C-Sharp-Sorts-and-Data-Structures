c909230b35808003b98f60511fbf15407de83b0e
Nordic-Beast <austin.forsling@gmail.com>
2015-09-23 20:59:36 -0600

    Added InsertionSorter

---
+++ SortsAndDataStructures/Sorts/InsertionSorter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SortsAndDataStructures{
+    static class InsertionSorter{
+        static public int[] sort(int[] ints){
+            for (int i = 1; i < ints.Length; i++){
+                int positionIndex = i;
+                while (positionIndex > 0 && (ints[positionIndex] < ints[positionIndex - 1])){
+                    SorterTools.swap(ints, positionIndex - 1, positionIndex);
+                    positionIndex--;
+                }
+            }
+            return ints;
+        }
+    }
+}
