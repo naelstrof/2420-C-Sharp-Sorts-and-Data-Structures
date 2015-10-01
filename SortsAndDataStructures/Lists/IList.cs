fb12e0f72980e42acd9f06dd0370b6846c7c8b78
naelstrof <naelstrof@gmail.com>
2015-10-01 15:00:53 -0700

    List work

---
+++ SortsAndDataStructures/Lists/IList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortsAndDataStructures {
+	public interface IList<Type> : ICollection<Type>, IEnumerable<Type> {
+		Type this[int index] { get; set; }
+		void Add (int value);
+		void ResizeIfNecessary ();
+		int IndexOf (Type value);
+		void RemoveAt( int index );
+		void Insert( int index, Type value );
+    }
+}
+
+
